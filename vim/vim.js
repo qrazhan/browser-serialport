2edab8ad386af0a9f9b13de563fd0114c0aa4c2f
Prashan Dharmasena <prashan14@gmail.com>
2015-09-14 00:47:51 -0700

    stuffs is working

--- vim/vim.js
+++ vim/vim.js
@@ -1,6 +1,14 @@
 var SerialPortLib = require('../index.js');
 var SerialPort = SerialPortLib.SerialPort;
 
+var array1 = ['A', 'F', 'K', 'P', 'U', 'Z'];
+var array5 = ['B', 'G', 'L', 'Q', 'V', '.'];
+var array2 = ['C', 'H', 'M', 'R', 'W', '!'];
+var array4 = ['D', 'I', 'N', 'S', 'X', '?'];
+var array3 = ['E', 'J', 'O', 'T', 'Y', '@'];
+
+var index = -1;
+
 SerialPortLib.list(function(err, ports) {
 	var portsPath = document.getElementById("portPath");
 
@@ -39,27 +47,8 @@ function connect(port, baudrate) {
 	    buffersize: 1
 	}, true);
 
-	var row = 0;
-	var col = 0;
-	var previous = document.getElementById("00");
-
 	var output = document.getElementById("output");
-	var movement = document.getElementById("movement");
-	movement.value = parseInt(0);
 	document.getElementById("settings").style.display = "none";
-
-	var setCell = function() {
-		var r = row.toString();
-		var c = col.toString();
-		console.log(r);
-		console.log(c);
-		console.log("====");
-		console.log(r + c);
-		var d = document.getElementById(r + c);
-		console.log(d);
-		previous = d;
-		d.className = "success";
-	};
 
 	sp.on("open", function() {
 		document.getElementById("connected-container").style.display = "block";
@@ -73,57 +62,70 @@ function connect(port, baudrate) {
 		output.textContent += "\nError: " + string + "\n";
 	});
 
+	var clearSelection = function(){
+		for(var i =1; i<7; i++){
+			var e = document.getElementById("big"+i);
+			e.className = "big-menu-item";	
+		}
+	};
+
+	var atime = new Date().getTime();
+	var btime= new Date().getTime();
+	var ctime= new Date().getTime();
+	var dtime= new Date().getTime();
+	var etime= new Date().getTime();
+	var ftime= new Date().getTime();
+
 	sp.on("data", function(data) {
 		var command = data.toString();
 		console.log(command);
-		if (command == "u") {
-			movement.value = parseInt(movement.value) + 1;
-		} else if (command == "d") {
-			movement.value = parseInt(movement.value) - 1;
-		} else if (command == "s") {
-			output.textContent += " ";
-		} else if (command == "e") {
-			var r = row.toString();
-			var c = col.toString();
-
-			output.textContent += document.getElementById(r + c).innerHTML;
-			movement.value = parseInt(0);
-		} else if (command == "h") {
-			previous.className = "";
-
-			col = parseInt(col) - parseInt(movement.value);
-			if (col < 0) {
-				col = 0;
+		if(command == "1" || command == "2" || command == "3" || command == "4" || command == "5" || command == "6"){
+			index = parseInt(command);
+			clearSelection();
+			switch(index){
+				case 1: 
+					var e = document.getElementById("big1");
+					e.className = e.className + " menu_selected";
+					break;
+				case 2: 
+					var e = document.getElementById("big3");
+					e.className = e.className + " menu_selected";
+					break;
+				case 3: 
+					var e = document.getElementById("big5");
+					e.className = e.className + " menu_selected";
+					break;
+				case 4: 
+					var e = document.getElementById("big6");
+					e.className = e.className + " menu_selected";
+					break;
+				case 5: 
+					var e = document.getElementById("big4");
+					e.className = e.className + " menu_selected";
+					break;
+				case 6: 
+					var e = document.getElementById("big2");
+					e.className = e.className + " menu_selected";
+					break;					
 			}
-
-			setCell();
-		} else if (command == "j") {
-			previous.className = "";
-
-			row = parseInt(row) + parseInt(movement.value);
-			if (row > 4) {
-				row = 4;
+			index -= 1;
+		} else if(command == "0"){
+			index = -1;
+			clearSelection();
+		} else if(command == "a" || command == "b" || command == "c" || command == "d" || command == "e" || command == "f"){
+			if(index == -1){
+				output.textContent += " ";
+			} else {
+				var time = new Date().getTime();
+				switch(command){
+					case "a": if(time-atime > 150) {output.textContent += array1[index];atime = time;} break;
+					case "b": if(time-btime > 150) {output.textContent += array2[index];btime = time;} break;
+					case "c": if(time-ctime > 150) {output.textContent += array3[index];ctime = time;} break;
+					case "d": if(time-dtime > 150) {output.textContent += array4[index];dtime = time;} break;
+					case "e": if(time-etime > 150) {output.textContent += array5[index];etime = time;} break;
+					case "f": if(time-ftime > 150) {output.textContent += " ";ftime = time;} break;
+				}
 			}
-
-			setCell();
-		} else if (command == "k") {
-			previous.className = "";
-
-			row = parseInt(row) - parseInt(movement.value);
-			if (row < 0) {
-				row = 0;
-			}
-
-			setCell();
-		} else if (command == "l") {
-			previous.className = "";
-
-			col = parseInt(col) + parseInt(movement.value);
-			if (col > 5) {
-				col = 5;
-			}
-
-			setCell();
 		}
 	});
 }
